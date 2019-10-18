b5e337b881f1a9cd5c3c6bbaf50ae7a3f710cac9
conradoRC <conrado27cym@gmail.com>
2019-10-18 12:53:25 -0500

    todo bien hasta ahora

--- src/Contoller/Controller_interface.java
+++ src/Contoller/Controller_interface.java
@@ -22,15 +22,15 @@ public class Controller_interface implements KeyListener {
 	private JTextField txt2[];
 	private int un[];
 	private int size=0;
-	private int [] enviado, recibido;
-	private boolean  [] flags, flags1;
+	String paridadd[];
+	private boolean [] enviado, recibido;
 	public Controller_interface(Interface in) {
 		this.in= in;
 	}
 	
 	@Override
 	public void keyReleased(KeyEvent e) {
-		in.mensajeFinal.setText("Mensaje recibido correctamente");
+		
 		if(e.getSource() == in.txt[0]) {
 			String m = in.txt[0].getText();
 			size = m.length();
@@ -43,22 +43,27 @@ public class Controller_interface implements KeyListener {
 			addCeldasB(size, destino);
 			in.txt[1].setText(cadenaBinari);
 			in.txt[2].setText(Arrays.toString(un));
+			in.mensajeFinal.setText("Mensaje recibido correctamente");
+		}	
+		
+		for (int i = 0; i < cadenaBinaria.length; i++) {
 			
-			
-			isPar(destino);
-			if(verification(recibido,un)) {
-				in.mensajeFinal.setText("Mensaje recibido correctamente");
-			}
 		}
-			
-		else {
+		
 			for (int c = 0; c < size; c++) {
 				if(e.getSource() == txt1[c]) {
 					for (int d = 0; d < cadenaBinaria.length; d++)
 						destino [d]= removeBit(txt1[d].getText()); 
 						addCeldasB(size, destino);
+						
+						if(verificar(strBinary,destino)) {
+							in.mensajeFinal.setText("Mensaje no recibido correctamente");
+						}
+						else {
+							in.mensajeFinal.setText("Mensaje recibido correctamente");
+						}
 				}
-			}
+			
 		}
 	}
 
@@ -69,10 +74,12 @@ public class Controller_interface implements KeyListener {
 	
 	public void addCeldas(int length) {
 		txt1 = new JTextField[length];
+		paridadd = new String [txt1.length];
 		in.panel_paridad.removeAll();
 		in.panel_paridad.repaint();
 		for (int c = 0; c < length; c++) {
 			txt1[c] = new JTextField(cadenaBinaria[c]);
+			paridadd[c] = cadenaBinaria[c].substring(0, cadenaBinaria[c].length()-1);
 			txt1[c].setFont(new Font("Cambria", Font.PLAIN, 14));
 			txt1[c].setBounds(c*75+3, 1, 70, 28);
 			txt1[c].setBorder(new LineBorder(Color.WHITE));
@@ -102,7 +109,7 @@ public class Controller_interface implements KeyListener {
 	public String cadenaABinario(String parCadena){
 	    int longitud = parCadena.length();
 	    cadenaBinaria=new String [longitud];
-	    enviado = new int[longitud];
+	    recibido = new boolean [longitud];
 	    strBinary=new String [longitud];
 	    String resultado="";
 	    cadenaBinari ="";
@@ -110,16 +117,16 @@ public class Controller_interface implements KeyListener {
 	    // System.out.println("longitud="+longitud+" longitud cadeBinaria"+cadenaBinaria.length +" longitud un"+ un.length);
 	    for(int i=0;i<longitud;i++){
 	       cadenaBinaria[i]= String.format("%8s", Integer.toBinaryString(parCadena.charAt(i)));
-	       cadenaBinari+= String.format("%10s", Integer.toBinaryString(parCadena.charAt(i)));
-	       strBinary[i]= String.format("%10s", Integer.toBinaryString(parCadena.charAt(i)));
+	       cadenaBinari+= String.format("%8s", Integer.toBinaryString(parCadena.charAt(i)));
+	       strBinary[i]= String.format("%8s", Integer.toBinaryString(parCadena.charAt(i)));
 	       un[i]=Integer.bitCount(parCadena.charAt(i));
 	       if(un[i]%2==0) {
 	    	   cadenaBinaria[i]+="0";
-	    	   enviado[i]= 0;
+	    	   recibido[i]= true;
 	       }
 	       else {
 	    	   cadenaBinaria[i]+="1";
-	    	   enviado[i]= 1;
+	    	   recibido[i]= false;
 	       }
 	       resultado+=cadenaBinaria[i];
 	    } 
@@ -128,27 +135,39 @@ public class Controller_interface implements KeyListener {
 	    //System.out.println("Bit de paridad agregado: "+Arrays.toString(cadenaBinaria));
 	    return resultado;
      }	
-
 	
-	  public void isPar(String [] destino) {
-		  recibido = new int[destino.length];
-		  
-		  for (int c = 0; c < destino.length; c++) {
-			  for (int i = 0; i < destino.length; i++) {
-				  if(destino[c].charAt(i)==('1')) {
-						recibido[c]= recibido[c]+1;
-			      }
-			  }
-		  }
-		  System.out.println(Arrays.toString(recibido));
-		  
-		  
-	  }
 	
-	 public boolean verification(int [] f, int [] g) {
-		 boolean flag= Arrays.equals(f, g);
-		 return flag;
-	 }
+	public boolean verificar(String [] a, String[] b) {
+		enviado = new boolean[a.length];
+		System.out.println(Arrays.toString(a));
+		boolean flag= false;
+		boolean bandera = true;
+		for (int i = 0; i < b.length; i++) {
+			int size = a[i].length();
+			for (int j = 0; j < size; j++) {
+				if(b.length<a.length) {
+					char aa= a[i].charAt(j);
+					char bb = b[i].charAt(j);
+					System.out.println(aa+"::" + bb);
+					if(aa==bb)
+					flag=true;
+				}
+			}
+		}
+		int count=0;
+		for (int i = 0; i < enviado.length; i++) {
+			if(enviado[i]==true) {
+				count++;
+			}
+		}
+		
+		if(count==a.length) {
+			bandera=false;
+		}
+		
+		return bandera;
+		
+	}
 		@Override
 		public void keyPressed(KeyEvent e) {
 		}
